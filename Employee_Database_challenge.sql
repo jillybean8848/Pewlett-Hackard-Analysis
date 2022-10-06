e2e0b7cb81451698c964772d5de0d26138bf8198
jillybean8848 <110632671+jillybean8848@users.noreply.github.com>
2022-10-06 02:30:12 -0400

    Add files via upload

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,57 @@
+--Deliverable 1
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+SELECT * FROM retirement_titles;
+
+--Check for duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no, to_date DESC;
+
+SELECT COUNT(ut.emp_no), ut.title
+INTO retirement_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT count(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+
+--Deliverable 2
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+
+SELECT * FROM mentorship_eligibility
+
+
+
